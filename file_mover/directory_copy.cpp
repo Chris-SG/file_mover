b696b5cc4887e7a0850f118354d30211e77ac7ef
Chris-SG <chris.shandgost@gmail.com>
2018-11-20 16:52:07 +1100

    Initial commit

---
+++ file_mover/directory_copy.cpp
@@ -0,0 +1,55 @@
+#include "pch.h"
+#include "directory_copy.hpp"
+
+namespace fs = std::filesystem;
+
+directory_copy::directory_copy(fs::path inpath)
+{
+	a_inpath = inpath;
+}
+
+bool directory_copy::copy(std::string share, std::string user, std::string password, std::string to_path)
+{
+	auto open_code = open_connection(const_cast<LPSTR>(share.c_str()), const_cast<LPSTR>(user.c_str()), const_cast<LPSTR>(password.c_str()));
+	if (open_code == ERROR_SESSION_CREDENTIAL_CONFLICT)
+		std::cout << "Connection pre-established for " << share << "." << std::endl;
+	else if (open_code != NO_ERROR)
+	{
+		LPSTR buf = nullptr;
+		FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, open_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&buf, 0, nullptr);
+		std::cout << "Failed to open connection " << share << ". Error code: " << open_code << " " << buf <<  std::endl;
+		close_connection(const_cast<LPSTR>(share.c_str()));
+
+		return false;
+	}
+	else
+		std::cout << "Connection established for " << share << "." << std::endl;
+
+	try
+	{
+		std::cout << "Copying files. Please wait..." << std::endl;
+		fs::copy(a_inpath, fs::path(share + to_path), fs::copy_options::update_existing | fs::copy_options::recursive);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	close_connection(const_cast<LPSTR>(share.c_str()));
+	return true;
+}
+
+DWORD directory_copy::open_connection(LPSTR path, LPSTR user, LPSTR password)
+{
+	NETRESOURCEA resource;
+	resource.dwType = RESOURCETYPE_DISK;
+	resource.lpLocalName = 0;
+	resource.lpRemoteName = path;
+	resource.lpProvider = 0;
+	return WNetAddConnection2A(&resource, password, user, CONNECT_TEMPORARY);
+}
+
+DWORD directory_copy::close_connection(LPSTR path)
+{
+	return WNetCancelConnection2A(path, 0, 1);
+}
---
+++ file_mover/directory_copy.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "pch.h"
+
+struct destination
+{
+	std::string share;
+	std::string user;
+	std::string password;
+	std::string path;
+};
+
+class directory_copy
+{
+private:
+	std::filesystem::path a_inpath;
+
+	DWORD open_connection(LPSTR path, LPSTR user, LPSTR password);
+	DWORD close_connection(LPSTR path);
+
+public:
+	directory_copy(std::filesystem::path inpath);
+	bool copy(std::string share, std::string user, std::string password, std::string to_path);
+};
