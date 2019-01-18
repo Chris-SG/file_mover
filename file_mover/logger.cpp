8054338d387af3d923aa61cf50a1e6a1b2e8509a
Chris-SG <chris.shandgost@gmail.com>
2019-01-18 17:02:48 +1100

    Add basic date formatting

--- file_mover/logger.cpp
+++ file_mover/logger.cpp
@@ -43,5 +43,99 @@ bool logger::write(std::string write)
 	if (logfile.is_open())
 	{
 		logfile << "[" << time_now() << "]" << write << "\n";
+		return true;
 	}
+	return false;
+}
+
+
+
+std::string logger::format_time(std::chrono::hours hr, std::string fmt)
+{
+	static uint32_t epoch_year = 1970;
+	uint32_t years = 0;
+	uint32_t hours = hr.count() % 24;
+	uint32_t days = ((hr.count() - hours) / 24) + 1;
+	uint32_t months = 1;
+	bool complete = false;
+	while (!complete)
+	{
+		if (is_leap_year(epoch_year + years))
+		{
+			if (days > 366)
+			{
+				++years;
+				days -= 366;
+			}
+			else
+				complete = true;
+		}
+		else
+		{
+			if (days > 365)
+			{
+				++years;
+				days -= 365;
+			}
+			else
+				complete = true;
+		}
+	}
+
+	std::initializer_list<uint8_t> months_30({ 4, 6, 9, 11 });
+	std::initializer_list<uint8_t> months_31({ 1, 3, 5, 7, 8, 10, 12 });
+
+	while (days > 28)
+	{
+		if (std::find(std::begin(months_30), std::end(months_30), months))
+		{
+			if (days <= 30)
+				break;
+			else
+			{
+				days -= 30;
+				++months;
+			}
+		}
+		else if (std::find(std::begin(months_31), std::end(months_31), months))
+		{
+			if (days <= 31)
+				break;
+			else
+			{
+				days -= 31;
+				++months;
+			}
+		}
+		else
+		{
+			if (is_leap_year(years + epoch_year) && days > 29)
+			{
+				days -= 29;
+				++months;
+			}
+			else if (days > 28)
+			{
+				days -= 29;
+				++months;
+			}
+		}
+	}
+
+	fmt.replace(fmt.find("%Y"), 2, std::to_string(epoch_year + years));
+	fmt.replace(fmt.find("%m"), 2, std::to_string(months));
+	fmt.replace(fmt.find("%d"), 2, std::to_string(days));
+
+	return fmt;
+}
+
+bool logger::is_leap_year(uint32_t year)
+{
+	if (year % 4 != 0)
+		return false;
+	else if (year % 100 != 0)
+		return true;
+	else if (year % 400 != 0)
+		return false;
+	return true;
 }
--- file_mover/logger.hpp
+++ file_mover/logger.hpp
@@ -7,10 +7,13 @@ namespace logger
 	static std::ofstream logfile = std::ofstream();
 
 	std::string time_now();
-
 	bool open(std::string log);
-
 	bool close();
-
 	bool write(std::string write);
+
+	std::string format_time(std::chrono::milliseconds ms, std::string fmt);
+	std::string format_time(std::chrono::seconds sec, std::string fmt);
+	std::string format_time(std::chrono::minutes min, std::string fmt);
+	std::string format_time(std::chrono::hours hr, std::string fmt);
+	bool is_leap_year(uint32_t year);
 }
